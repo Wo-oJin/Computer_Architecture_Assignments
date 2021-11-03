3627a847fb19572e43e1837e96d0a1aa9cd8f8c9
Wo-oJin <dnwls813@ajou.ac.kr>
2021-11-03 12:14:57 +0900

    pa2: Implement 'load_program' function

--- Practice/practice.c
+++ Practice/practice.c
@@ -1,100 +1,356 @@
-#define _CRT_SECURE_NO_WARNINGS
+/**********************************************************************
+ * Copyright (c) 2019-2021
+ *  Sang-Hoon Kim <sanghoonkim@ajou.ac.kr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTIABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ **********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <string.h>
-#include <ctype.h>
 #include <inttypes.h>
+#include <ctype.h>
 
-#define true 1
-#define false 0
+ /*====================================================================*/
+ /*          ****** DO NOT MODIFY ANYTHING FROM THIS LINE ******       */
 
-#define MAP_SIZE 50
-#define WORD_LEN 5
+ /* To avoid security error on Visual Studio */
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 4996)
 
-enum slotStatus { empty, deleted, inuse};
-typedef int (*hashfunc)(char* key);
+#define MAX_NR_TOKENS	32	/* Maximum length of tokens in a command */
+#define MAX_TOKEN_LEN	64	/* Maximum length of single token */
+#define MAX_COMMAND	256 /* Maximum length of command string */
 
-typedef struct Slot { //Hash Map을 이루는 각 slot
-	char key[WORD_LEN];
-	int* val;
-	enum slotStatus status;
-}slot;
+typedef unsigned char bool;
+#define true	1
+#define false	0
 
-typedef struct HashMap { //Hash Map
-	slot bucket[MAP_SIZE];
-	hashfunc hf;
-}map;
 
-void mapInit(map* hm, hashfunc hf) {
-	for (int i = 0; i < MAP_SIZE; i++)
-		(hm->bucket[i]).status = empty;
-	hm->hf = hf;
-}
+/**
+ * memory[] emulates the memory of the machine
+ */
+static unsigned char memory[1 << 20] = {	/* 1MB memory at 0x0000 0000 -- 0x0100 0000 */
+	0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+	0xde, 0xad, 0xbe, 0xef, 0x00, 0x00, 0x00, 0x00,
+	'h',  'e',  'l',  'l',  'o',  ' ',  'w',  'o',
+	'r',  'l',  'd',  '!',  '!',  0x00, 0x00, 0x00,
+	'a',  'w',  'e',  's',  'o',  'm',  'e',  ' ',
+	'c',  'o',  'm',  'p',  'u',  't',  'e',  'r',
+	' ',  'a',  'r',  'c',  'h',  'i',  't',  'e',
+	'c',  't',  'u',  'r',  'e',  '!',  0x00, 0x00,
+};
 
-int makeHash(char* str) { //인자로 전달된 key값을 hash value로 전환
-	int hash=0;
-	while(*str!='\0') {
-		hash += (int)(*str)%MAP_SIZE;
-		str++;
-	}
+#define INITIAL_PC	0x1000	/* Initial value for PC register */
+#define INITIAL_SP	0x8000	/* Initial location for stack pointer */
 
-	return hash % MAP_SIZE;
-}
+/**
+ * Registers of the machine
+ */
+static unsigned int registers[32] = {
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0x10, INITIAL_PC, 0x20, 3, 0xbadacafe, 0xcdcdcdcd, 0xffffffff, 7,
+	0, 0, 0, 0, 0, INITIAL_SP, 0, 0,
+};
 
-void insert(map* hm, char* key, int val1, int val2) { //Hash Map에 인자로 전달된 (key, value)를 저장
-	int* arr = (int*)malloc(sizeof(int) * 2);
-	arr[0] = val1; arr[1] = val2;
-	int hv = hm->hf(key);
+/**
+ * Names of the registers. Note that $zero is shorten to zr
+ */
+const char* register_names[] = {
+	"zr", "at", "v0", "v1", "a0", "a1", "a2", "a3",
+	"t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
+	"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
+	"t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"
+};
 
-	while (hm->bucket[hv].status == inuse) {
-		hv++;
-		hv %= MAP_SIZE;
-	}
+/**
+ * Program counter register
+ */
+static unsigned int pc = INITIAL_PC;
 
-	hm->bucket[hv].val=arr;
-	strcpy(hm->bucket[hv].key, key);
-	hm->bucket[hv].status = inuse;
-}
-
-int* search(map* hm, char* key) { // Hash Map에서 인자로 전달된 key에 대응되는 value을 반환
-	int hv = hm->hf(key);
-	int temp = hv;
-	while (strcmp(key, hm->bucket[hv].key)) {
-		hv++;
-		hv %= MAP_SIZE;
-
-		if (hv == temp)
-			return NULL;
-	}
-
-	return hm->bucket[hv].val;
-}
-
-void makeMap(map* hm) { // Hash Map 초기화 함수(opcode/funct number, register number)
-	mapInit(hm, makeHash);
-	int* arr;
-
-	// opcode/funct
-	insert(hm, "add", 0, 0x20); insert(hm, "addi", 0x08, 0); insert(hm, "andi", 0x0c, 0); insert(hm, "or", 0, 0x25);
-	insert(hm, "sub", 0, 0x22); insert(hm, "and", 0, 0x24); insert(hm, "ori", 0x0d, 0); insert(hm, "nor", 0, 0x27);
-	insert(hm, "sll", 0, 0x00); insert(hm, "srl", 0, 0x02); insert(hm, "sra", 0, 0x03); insert(hm, "lw", 0x23,0);
-	insert(hm, "sw", 0x2b,0); 
-
-	// register number
-	insert(hm, "zero", 0, 0); insert(hm, "at", 1, 0); insert(hm, "v0", 2, 0); insert(hm, "v1", 3, 0);
-	insert(hm, "a0", 4, 0); insert(hm, "a1", 5, 0); insert(hm, "a2", 6, 0); insert(hm, "a3", 7, 0);
-	insert(hm, "t0", 8, 0); insert(hm, "t1", 9, 0); insert(hm, "t2", 10, 0); insert(hm, "t3", 11, 0);
-	insert(hm, "t4", 12, 0); insert(hm, "t5", 13, 0); insert(hm, "t6", 14, 0); insert(hm, "t7", 15, 0);
-	insert(hm, "s0", 16, 0); insert(hm, "s1", 17, 0); insert(hm, "s2", 18, 0); insert(hm, "s3", 19, 0);
-	insert(hm, "s4", 20, 0); insert(hm, "s5", 21, 0); insert(hm, "s6", 22, 0); insert(hm, "s7", 23, 0);
-	insert(hm, "t8", 24, 0); insert(hm, "t9", 25, 0); insert(hm, "k1", 26, 0); insert(hm, "k2", 27, 0);
-	insert(hm, "gp", 28, 0); insert(hm, "sp", 29, 0); insert(hm, "fp", 30, 0); insert(hm, "ra", 31, 0); 
-}
-
-static int parse_command(char* assembly, int* nr_tokens, char* tokens[]) //사용자가 입력한 어셈블리어를 white space를 기준으로 split
+/**
+ * strmatch()
+ *
+ * DESCRIPTION
+ *   Compare strings @str and @expect and return 1 if they are the same.
+ *   You may use this function to simplify string matching :)
+ *
+ * RETURN
+ *   1 if @str and @expect are the same
+ *   0 otherwise
+ */
+static inline bool strmatch(char* const str, const char* expect)
 {
-	char* curr = assembly;
+	return (strlen(str) == strlen(expect)) && (strncmp(str, expect, strlen(expect)) == 0);
+}
+
+/*          ****** DO NOT MODIFY ANYTHING UP TO THIS LINE ******      */
+/*====================================================================*/
+
+
+/**********************************************************************
+ * process_instruction
+ *
+ * DESCRIPTION
+ *   Execute the machine code given through @instr. The following table lists
+ *   up the instructions to support. Note that a pseudo instruction 'halt'
+ *   (0xffffffff) is added for the testing purpose. Also '*' instrunctions are
+ *   the ones that are newly added to PA2.
+ *
+ * | Name   | Format    | Opcode / opcode + funct |
+ * | ------ | --------- | ----------------------- |
+ * | `add`  | r-format  | 0 + 0x20                |
+ * | `addi` | i-format  | 0x08                    |
+ * | `sub`  | r-format  | 0 + 0x22                |
+ * | `and`  | r-format  | 0 + 0x24                |
+ * | `andi` | i-format  | 0x0c                    |
+ * | `or`   | r-format  | 0 + 0x25                |
+ * | `ori`  | i-format  | 0x0d                    |
+ * | `nor`  | r-format  | 0 + 0x27                |
+ * | `sll`  | r-format  | 0 + 0x00                |
+ * | `srl`  | r-format  | 0 + 0x02                |
+ * | `sra`  | r-format  | 0 + 0x03                |
+ * | `lw`   | i-format  | 0x23                    |
+ * | `sw`   | i-format  | 0x2b                    |
+ * | `slt`  | r-format* | 0 + 0x2a                |
+ * | `slti` | i-format* | 0x0a                    |
+ * | `beq`  | i-format* | 0x04                    |
+ * | `bne`  | i-format* | 0x05                    |
+ * | `jr`   | r-format* | 0 + 0x08                |
+ * | `j`    | j-format* | 0x02                    |
+ * | `jal`  | j-format* | 0x03                    |
+ * | `halt` | special*  | @instr == 0xffffffff    |
+ *
+ * RETURN VALUE
+ *   1 if successfully processed the instruction.
+ *   0 if @instr is 'halt' or unknown instructions
+ */
+static int process_instruction(unsigned int instr)
+{
+	return 0;
+}
+
+
+/**********************************************************************
+ * load_program
+ *
+ * DESCRIPTION
+ *   Load the instructions in the file @filename onto the memory starting at
+ *   @INITIAL_PC. Each line in the program file looks like;
+ *
+ *	 [MIPS instruction started with 0x prefix]  // optional comments
+ *
+ *   For example,
+ *
+ *   0x8c090008
+ *   0xac090020	// sw t1, zero + 32
+ *   0x8c080000
+ *
+ *   implies three MIPS instructions to load. Each machine instruction may
+ *   be followed by comments like the second instruction. However you can simply
+ *   call strtoimax(linebuffer, NULL, 0) to read the machine code while
+ *   ignoring the comment parts.
+ *
+ *	 The program DOES NOT include the 'halt' instruction. Thus, make sure the
+ *	 'halt' instruction is appended to the loaded instructions to terminate
+ *	 your program properly.
+ *
+ *	 Refer to the @main() for reading data from files. (fopen, fgets, fclose).
+ *
+ * RETURN
+ *	 0 on successfully load the program
+ *	 any other value otherwise
+ */
+
+static int __parse_command(char* command, int* nr_tokens, char* tokens[]);
+
+static int load_program(char* const filename)
+{
+	FILE* input = fopen(filename, "r");
+
+	if (!input){
+		printf("error!\n");
+		return -EINVAL;
+	}
+
+	char command[MAX_COMMAND] = { '\0' };
+	unsigned int tp_pc = pc;
+	while (fgets(command, sizeof(command), input)) {
+		char* tokens[MAX_NR_TOKENS] = { NULL };
+		int nr_tokens = 0;
+
+		for (size_t i = 0; i < strlen(command); i++) {
+			command[i] = tolower(command[i]);
+		}
+
+		if (__parse_command(command, &nr_tokens, tokens) < 0)
+			continue;
+
+		unsigned int decimal = strtoimax(tokens[0], NULL, 16);
+		int hexa[8] = { 0 };
+
+		long div;
+		int idx = 7;
+		while (decimal > 0) {
+			div = decimal % 16;
+			decimal /= 16;
+
+			hexa[idx--] = div;
+		}
+
+		while (idx >= 0)
+			hexa[idx--] = 0;
+
+		for (int i = 3, k = 7; i >= 0; i--, k -= 2)
+			memory[tp_pc + i] = (hexa[k - 1] * 16) + hexa[k];
+
+		tp_pc += 4;
+	}
+
+	for (int i = 3; i >= 0; i--)
+		memory[tp_pc + i] = 0xff;
+
+	return 0;
+}
+
+
+/**********************************************************************
+ * run_program
+ *
+ * DESCRIPTION
+ *   Start running the program that is loaded by @load_program function above.
+ *   If you implement @load_program() properly, the first instruction is placed
+ *   at @INITIAL_PC. Using @pc, which is the program counter of this processor,
+ *   you can emulate the MIPS processor by
+ *
+ *   1. Read instruction from @pc
+ *   2. Increment @pc by 4
+ *   3. Call @process_instruction(instruction)
+ *   4. Repeat until @process_instruction() returns 0
+ *
+ * RETURN
+ *   0
+ */
+static int run_program(void)
+{
+	pc = INITIAL_PC;
+
+	return 0;
+}
+
+
+
+/*====================================================================*/
+/*          ****** DO NOT MODIFY ANYTHING FROM THIS LINE ******       */
+static void __show_registers(char* const register_name)
+{
+	int from = 0, to = 0;
+	bool include_pc = false;
+
+	if (strmatch(register_name, "all")) {
+		from = 0;
+		to = 32;
+		include_pc = true;
+	}
+	else if (strmatch(register_name, "pc")) {
+		include_pc = true;
+	}
+	else {
+		for (int i = 0; i < sizeof(register_names) / sizeof(*register_names); i++) {
+			if (strmatch(register_name, register_names[i])) {
+				from = i;
+				to = i + 1;
+			}
+		}
+	}
+
+	for (int i = from; i < to; i++) {
+		fprintf(stderr, "[%02d:%2s] 0x%08x    %u\n", i, register_names[i], registers[i], registers[i]);
+	}
+	if (include_pc) {
+		fprintf(stderr, "[  pc ] 0x%08x\n", pc);
+	}
+}
+
+static void __dump_memory(unsigned int addr, size_t length)
+{
+	for (size_t i = 0; i < length; i += 4) {
+		fprintf(stderr, "0x%08lx:  %02x %02x %02x %02x    %c %c %c %c\n",
+			addr + i,
+			memory[addr + i], memory[addr + i + 1],
+			memory[addr + i + 2], memory[addr + i + 3],
+			isprint(memory[addr + i]) ? memory[addr + i] : '.',
+			isprint(memory[addr + i + 1]) ? memory[addr + i + 1] : '.',
+			isprint(memory[addr + i + 2]) ? memory[addr + i + 2] : '.',
+			isprint(memory[addr + i + 3]) ? memory[addr + i + 3] : '.');
+	}
+}
+
+static void __process_command(int argc, char* argv[])
+{
+	if (argc == 0) return;
+
+	if (strmatch(argv[0], "load")) {
+		if (argc == 2) {
+			load_program(argv[1]);
+		}
+		else {
+			printf("Usage: load [program filename]\n");
+		}
+	}
+	else if (strmatch(argv[0], "run")) {
+		if (argc == 1) {
+			run_program();
+		}
+		else {
+			printf("Usage: run\n");
+		}
+	}
+	else if (strmatch(argv[0], "show")) {
+		if (argc == 1) {
+			__show_registers("all");
+		}
+		else if (argc == 2) {
+			__show_registers(argv[1]);
+		}
+		else {
+			printf("Usage: show { [register name] }\n");
+		}
+	}
+	else if (strmatch(argv[0], "dump")) {
+		if (argc == 3) {
+			__dump_memory(strtoimax(argv[1], NULL, 0), strtoimax(argv[2], NULL, 0));
+		}
+		else {
+			printf("Usage: dump [start address] [length]\n");
+		}
+	}
+	else {
+		/**
+		 * You may hook up @translate() from pa1 here to allow assembly input!
+		 */
+#ifdef INPUT_ASSEMBLY
+		unsigned int instr = translate(argc, argv);
+		process_instruction(instr);
+#else
+		process_instruction(strtoimax(argv[0], NULL, 0));
+#endif
+	}
+}
+
+static int __parse_command(char* command, int* nr_tokens, char* tokens[])
+{
+	char* curr = command;
 	int token_started = false;
 	*nr_tokens = 0;
 
@@ -113,187 +369,63 @@ static int parse_command(char* assembly, int* nr_tokens, char* tokens[]) //사용�
 		curr++;
 	}
 
+	/* Exclude comments from tokens */
+	for (int i = 0; i < *nr_tokens; i++) {
+		if (strmatch(tokens[i], "//") || strmatch(tokens[i], "#")) {
+			*nr_tokens = i;
+			tokens[i] = NULL;
+		}
+	}
+
 	return 0;
 }
 
-void ten_to_two(int* instruction, int n, int size, int* idx) { //전달된 크기만큼의 인덱스를 갖는 10진수 -> 2진수 변환
-	int* arr = (int*)malloc(sizeof(int) * size);
-	int tp = size;
-	char negative = false;
-	size -= 1;
+int main(int argc, char* const argv[])
+{
+	char command[MAX_COMMAND] = { '\0' };
+	FILE* input = stdin;
 
-	if (n < 0) {
-		n *= -1;
-		negative = true;
-	}
-
-	while (n >= 1) {
-		arr[size--] = n % 2;
-		n >>= 1;
-	}
-
-	while (size >= 0) 
-		arr[size--] = 0;
-
-	for (int k = 0; k < tp; k++) {
-		if(negative)
-			instruction[(*idx)++] = !arr[k];
-		else
-			instruction[(*idx)++] = arr[k];
-	}
-}
-
-char ten_to_six(int n) { //0~15 범위의 10진수 -> 16진수로 변환
-	switch (n) {
-	case 10:
-		return 'a';
-	case 11:
-		return 'b';
-	case 12:
-		return 'c';
-	case 13:
-		return 'd';
-	case 14:
-		return 'e';
-	case 15:
-		return 'f';
-	default:
-		return n+'0';
-	}
-}
-
-int strtonum(char* num) { //I_format, shift 어셈블리어에 포함된 10진수, 16진수 string 타입의 상수를 알맞은 정수로 반환 
-	char* ptr;
-
-	if (num[0] == '-') {
-		if (num[1] == '0')
-			return strtoimax(num, &ptr, 16) + 1;
-		else
-			return strtoimax(num, &ptr, 10) + 1;
-	}
-	else {
-		if (num[0] == '0')
-			return strtoimax(num, &ptr, 16);
-		else
-			return strtoimax(num, &ptr, 10);
-	}
-}
-
-// op(6) rs(5) rt(5) rd(5) shamt(5) funct(6)
-void makeRformat(map* hm, int* r_format, char* tokens[], int shamt) { //어셈블리어 -> R_format 변환
-	char flag = false;
-	if (shamt > 0)
-		flag = true;
-	int* op_funct = search(hm, tokens[0]);
-	r_format[0] = op_funct[0]; 
-
-	int* val = search(hm, tokens[2]);
-	if (!flag) 
-		r_format[1] = val[0];
-	else
-		r_format[2] = val[0];
-
-	val = search(hm, tokens[3]);
-	if(!flag)
-		r_format[2] = val[0];
-	else
-		r_format[1] = 0;
-	
-	val = search(hm, tokens[1]);
-	r_format[3] = val[0];
-
-	r_format[4] = shamt;
-	r_format[5] = op_funct[1];
-}
-
-// op(6) rs(5) rt(5) constant or address(16) 
-void makeIformat(map* hm, int* I_format, char* tokens[]) { //어셈블리어 -> I_format 변환
-	int* op_funct = search(hm, tokens[0]);
-	I_format[0] = op_funct[0];
-
-	int* val = search(hm, tokens[2]);
-	I_format[1] = val[0];
-
-	val = search(hm, tokens[1]);
-	I_format[2] = val[0];
-
-	I_format[3] = strtonum(tokens[3]);
-}
-
-int main(void) {
-	map hm;
-	makeMap(&hm);
-	int* format;
-	int format_size;
-	char flag = false;
-	char assembly[] = "addi sp sp -0x10";
-
-	int nr_tokens;
-	char** tokens = (char**)malloc(sizeof(char*) * 5);
-	parse_command(assembly, &nr_tokens, tokens);
-
-	if (search(&hm, tokens[3]) == NULL) {
-		if (!strcmp("sll", tokens[0]) || !strcmp("srl", tokens[0])) {
-			format_size = 6;
-			format = (int*)malloc(sizeof(int) * format_size);
-			makeRformat(&hm, format, tokens,strtonum(tokens[3])); //어셈블리어 -> R_format으로 변환
-		}
-		else {
-			flag = true;
-			format_size = 4;
-			format = (int*)malloc(sizeof(int) * format_size);
-			makeIformat(&hm, format, tokens); //어셈블리어 -> I_format으로 변환
-		}
-	}
-	else {
-		format_size = 6;
-		format = (int*)malloc(sizeof(int) * format_size);
-		makeRformat(&hm, format, tokens, 0); //어셈블리어 -> R_format으로 변환
-	}
-	for (int i = 0; i < format_size; i++)
-		printf("%d ", format[i]);
-	printf("\n");
-
-	int idx = 0;
-	int instruction[32];
-	int R_size[6] = { 6,5,5,5,5,6 };
-	int I_size[4] = { 6,5,5,16 };
-
-	if (!flag) {
-		for (int i = 0; i < format_size; i++) { //R_format의 각 필드값을 2진수로 전환
-			ten_to_two(instruction, format[i], R_size[i], &idx);
-		}
-	}
-	else {
-		for (int i = 0; i < format_size; i++) { //I_format의 각 필드값을 2진수로 전환
-			ten_to_two(instruction, format[i], I_size[i], &idx);
+	if (argc > 1) {
+		input = fopen(argv[1], "r");
+		if (!input) {
+			fprintf(stderr, "No input file %s\n", argv[0]);
+			return EXIT_FAILURE;
 		}
 	}
 
-	for (int i = 0; i < 32; i++)
-		printf("%d", instruction[i]);
-	printf("\n");
-
-	char OXinstruction[8];
-	int num = 0, a=8;
-
-	for (int i = 0, k = 0; i <= 32;) { //32자리의 2진수를 4자리씩 끊어서 8개의 16진수를 만듬
-		num += instruction[i] * a;
-		a >>= 1;
-		i++;
-		if (i % 4 == 0) {
-			a = 8;
-			OXinstruction[k++] = ten_to_six(num);
-			num = 0;
-		}
+	if (input == stdin) {
+		printf("*********************************************************\n");
+		printf("*          >> SCE212 MIPS Simulator v0.01 <<            *\n");
+		printf("*                                                       *\n");
+		printf("*                                       .---.           *\n");
+		printf("*                           .--------.  |___|           *\n");
+		printf("*                           |.------.|  | =.|           *\n");
+		printf("*                           || >>_  ||  |---|           *\n");
+		printf("*                           |'------'|  |   |           *\n");
+		printf("*                           ')______('~~|___|           *\n");
+		printf("*                                                       *\n");
+		printf("*                                   Fall 2021           *\n");
+		printf("*********************************************************\n\n");
+		printf(">> ");
 	}
 
-	for (int i = 0; i < 8; i++)
-		printf("%c ", OXinstruction[i]);
+	while (fgets(command, sizeof(command), input)) {
+		char* tokens[MAX_NR_TOKENS] = { NULL };
+		int nr_tokens = 0;
 
-	printf("\n");
+		for (size_t i = 0; i < strlen(command); i++) {
+			command[i] = tolower(command[i]);
+		}
 
-	char* ptr;
-	int n = strtoimax(OXinstruction, &ptr, 16); //문자열 형태의 instruction을 16진수로 int로 변환
-	printf("0x%08x\n", n);
+		if (__parse_command(command, &nr_tokens, tokens) < 0)
+			continue;
+
+		__process_command(nr_tokens, tokens);
+
+		if (input == stdin) printf(">> ");
+	}
+
+	if (input != stdin) fclose(input);
+
+	return EXIT_SUCCESS;
 }
